2f9ff3574e7485126eb52b16a4f1250fa14caa10
rsj <rishijarral@gmail.com>
2021-06-14 15:40:45 -0500

    git commit

--- merger.py
+++ merger.py
@@ -1,30 +1,31 @@
-from tkinter import *
+#from tkinter import *
 import pandas as pd
+from functools import reduce
+excel1 = input("Please enter excel1:\n")
+excel2 = input("Please enter excel2:\n")
+excel3 = input("Please enter excel3:\n")
+excel4 = input("Please enter excel4:\n")
+excel5 = input("Please enter excel5:\n")
+excel6 = input("Please enter excel6:\n")
+excel7 = input("Please enter excel7:\n")
+excel8 = input("Please enter excel8:\n")
+excel9 = input("Please enter excel8:\n")
 
-def submit_fields():
-    excel1 = entry1.get()
-    excel2 = entry2.get()
-    a = 'b.xlsx'
-    df1 = pd.read_excel(excel1,sheet_name=1)
- #   print(df1)
-    df2 = pd.read_excel(excel2,sheet_name=1) 
+df1 = pd.read_excel(excel1,sheet_name=1)
+df2 = pd.read_excel(excel2,sheet_name=1)
+df3 = pd.read_excel(excel3,sheet_name=1) 
+df4 = pd.read_excel(excel4,sheet_name=1) 
+df5 = pd.read_excel(excel5,sheet_name=1) 
+df6 = pd.read_excel(excel6,sheet_name=1) 
+df7 = pd.read_excel(excel7,sheet_name=1)  
+df8 = pd.read_excel(excel8,sheet_name=1)
+df9 = pd.read_excel(excel9,sheet_name=1)  
+df10 = [df1,df2,df3,df4,df5,df6,df7,df8,df9]
    # print(df1.get('Team','Subteam'))
-    df3 = df1[['EmployeeID','Team','Subteam','First Name','Last Name','DCPDS Employee Name','Function (job title)','CAO Shop Code','MAO Code','Source','ORG (NAVAIR or\nCompany name goes here)','COMML (phone number)','Email','Site(e.g. "FRCSW")','Room, WS, or Office (Room,\nCube or Office)','Building - Floor','Asset Status','Service ID','Asset Number','Machine Name','"System Model" (found by hitting the windows key and then typing system, and choosing "System Information")','Port','Additional PC assigned to user?','Scheduled for Tech Refresh?','Acrobat Pro','Visio','Project','Roxio\nSeat?','Date Last Validated Against NMCI Config\nReport','Results of Validation Against NMCI Config\nReport','Developer Seat?  If yes, please fill out the administrator column --------------->','Administrator for developer seat','4.0 NMCI POC Email','Notes']].merge(df2[['EmployeeID','Team','Subteam','First Name','Last Name','DCPDS Employee Name','Function (job title)','CAO Shop Code','MAO Code','Source','ORG (NAVAIR or\nCompany name goes here)','COMML (phone number)','Email','Site(e.g. "FRCSW")','Room, WS, or Office (Room,\nCube or Office)','Building - Floor','Asset Status','Service ID','Asset Number','Machine Name','"System Model" (found by hitting the windows key and then typing system, and choosing "System Information")','Port','Additional PC assigned to user?','Scheduled for Tech Refresh?','Acrobat Pro','Visio','Project','Roxio\nSeat?','Date Last Validated Against NMCI Config\nReport','Results of Validation Against NMCI Config\nReport','Developer Seat?  If yes, please fill out the administrator column --------------->','Administrator for developer seat','4.0 NMCI POC Email','Notes']])
-    df3.to_excel("merge.xlsx")
-    entry1.delete(0, END)
-    entry2.delete(0, END)
-root = Tk()
-Label(root, text="File1").grid(row=0)
-Label(root, text="File2").grid(row=1)
-entry1 = Entry(root)
-entry2 = Entry(root)
+#dat = 'EmployeeID','Team','Subteam','First Name','Last Name','DCPDS Employee Name','Function (job title)','CAO Shop Code','MAO Code','Source','ORG (NAVAIR or\nCompany name goes here)','COMML (phone number)','Email','Site(e.g. "FRCSW")','Room, WS, or Office (Room,\nCube or Office)','Building - Floor','Asset Status','Service ID','Asset Number','Machine Name','"System Model" (found by hitting the windows key and then typing system, and choosing "System Information")','Port','Additional PC assigned to user?','Scheduled for Tech Refresh?','Acrobat Pro','Visio','Project','Roxio\nSeat?','Date Last Validated Against NMCI Config\nReport','Results of Validation Against NMCI Config\nReport','Developer Seat?  If yes, please fill out the administrator column --------------->','Administrator for developer seat','4.0 NMCI POC Email','Notes']].merge(df2[['EmployeeID','Team','Subteam','First Name','Last Name','DCPDS Employee Name','Function (job title)','CAO Shop Code','MAO Code','Source','ORG (NAVAIR or\nCompany name goes here)','COMML (phone number)','Email','Site(e.g. "FRCSW")','Room, WS, or Office (Room,\nCube or Office)','Building - Floor','Asset Status','Service ID','Asset Number','Machine Name','"System Model" (found by hitting the windows key and then typing system, and choosing "System Information")','Port','Additional PC assigned to user?','Scheduled for Tech Refresh?','Acrobat Pro','Visio','Project','Roxio\nSeat?','Date Last Validated Against NMCI Config\nReport','Results of Validation Against NMCI Config\nReport','Developer Seat?  If yes, please fill out the administrator column --------------->','Administrator for developer seat','4.0 NMCI POC Email','Notes'
+#df11 = ((df1[[dat]]).merge(df2[[dat]])).merge()
+#df11 = reduce(lambda left,right: pd.concat(left,right,on=['EmployeeID','Team','Subteam','First Name','Last Name','DCPDS Employee Name','Function (job title)','CAO Shop Code','MAO Code','Source','ORG (NAVAIR or\nCompany name goes here)','COMML (phone number)','Email','Site(e.g. "FRCSW")','Room, WS, or Office (Room,\nCube or Office)','Building - Floor','Asset Status','Service ID','Asset Number','Machine Name','"System Model" (found by hitting the windows key and then typing system, and choosing "System Information")','Port','Additional PC assigned to user?','Scheduled for Tech Refresh?','Acrobat Pro','Visio','Project','Roxio\nSeat?','Date Last Validated Against NMCI Config\nReport','Results of Validation Against NMCI Config\nReport','Developer Seat?  If yes, please fill out the administrator column --------------->','Administrator for developer seat','4.0 NMCI POC Email','Notes']), df10)
+df11 = df1.append([df2,df3,df4,df5,df6,df7,df8,df9])
+df11.to_excel("merge.xlsx")
 
-entry1.grid(row=0, column=1)
-entry2.grid(row=1, column=1)
-
-Button(root, text='Quit', command=root.quit).grid(row=3,column=0, pady=4)
-Button(root, text='Submit', command=submit_fields).grid(row=3,column=1, pady=4)
-
-  
-root.mainloop()
 
